a16bdef2d66c0570cec5e06eb201df9e864b42bd
Rocco Paolillo <36846038+RoccoPaolillo@users.noreply.github.com>
2023-11-15 03:23:24 +0100

    ipf inp-exec-output

--- ipf.py
+++ ipf.py
@@ -9,34 +9,16 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import csv
+import os
 
-# with open('C:/Users/rocpa/OneDrive/Desktop/ROME_CNR/WP5/WP5_FOSSR-main/WP5_FOSSR-main/testcsv.csv', newline='') as csvfile:
-#    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-#    for row in spamreader:
-#        print(', '.join(row))
+# upload csv dataframe #####
 
-
-with open('C:/Users/rocpa/OneDrive/Desktop/ROME_CNR/WP5/WP5_FOSSR-main/WP5_FOSSR-main/testcsv.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
-
-np.loadtxt('C:/Users/rocpa/OneDrive/Desktop/ROME_CNR/WP5/WP5_FOSSR-main/WP5_FOSSR-main/test1.csv',delimiter = ";",skiprows = 1)
-
-
-with open('C:/Users/rocpa/OneDrive/Desktop/ROME_CNR/WP5/WP5_FOSSR-main/WP5_FOSSR-main/test1.csv') as f:
-    spamreader = csv.reader(f, delimiter=';')
-    reader = csv.reader(f)
-    lst = list(reader)
-    print(lst)
-    
-array = np.array(lst)
-print(array)    
-    
+# to import csv
 df = pd.read_csv('C:/Users/rocpa/OneDrive/Desktop/ROME_CNR/WP5/WP5_FOSSR-main/WP5_FOSSR-main/original_dataset2023agegen.csv',
                  header=None, delimiter = ",")
 array = df.to_numpy()    
 
+# to filter aggregated at national level ("Italia" region)
 filter_arr = []
 
 for element in array[:,3]:
@@ -48,10 +30,11 @@ for element in array[:,3]:
 
 array = array[filter_arr]
 
+# to filter out totals age
+
 filter_arr2 = []
 
 for element in array[:,9]:
-  # if the element is higher than 42, set the value to True, otherwise False:
   if element != 'Totale':
     filter_arr2.append(True)
   else:
@@ -59,10 +42,11 @@ for element in array[:,9]:
 
 array = array[filter_arr2]
 
+# to filter out totals gender
 filter_arr3 = []
 
 for element in array[:,7]:
-  # if the element is higher than 42, set the value to True, otherwise False:
+
   if element != 'Totale':
     filter_arr3.append(True)
   else:
@@ -70,9 +54,11 @@ for element in array[:,7]:
 
 array = array[filter_arr3]
 
+# to filter columns needed based on location (gender, age, observation)
 array = array[:,(7,9,13)]
-array[:,2] = array[:,2].astype(float)
+array[:,2] = array[:,2].astype(float) # to read observation as number (upload as csv)
 
+# to filter subset of male population
 filter_arrm = []
 
 for element in array[:,0]:
@@ -84,9 +70,11 @@ for element in array[:,0]:
 
 array_m = array[filter_arrm]
 
-array_m[0:51,:]
+array_m[0:51,:]  # marginals males
 array_m[51:81,:]
 array_m[81:101,:]
+
+# to filter subset of female population
 
 filter_arrf = []
 
@@ -99,37 +87,40 @@ for element in array[:,0]:
 
 array_f = array[filter_arrf]
 
-array_f[0:51,:]
+array_f[0:51,:]   # marginal femals
 array_f[51:81,:]
 array_f[81:101,:]
 
-
+# set target marginals
 TGTunder50 = array_m[0:51,2].sum() + array_f[0:51,2].sum()
 TGT51to80 = array_m[51:81,2].sum() + array_f[51:81,2].sum()
 TGTover80 = array_m[81:101,2].sum() + array_f[81:101,2].sum()
 TGTmale = array_m[0:51,2].sum() + array_m[51:81,2].sum() + array_m[81:101,2].sum()
 TGTfemale = array_f[0:51,2].sum() + array_f[51:81,2].sum() + array_f[81:101,2].sum()
 
+T = np.array([
+    [array_m[0:51,2].sum(),array_f[0:51,2].sum()],
+    [array_m[51:81,2].sum(),array_f[51:81,2].sum()],
+    [array_m[81:101,2].sum(),array_f[81:101,2].sum()]
+    ])
 
+
+####### algorithm IPF computation
+
+# artificial population (each cross-category (unknown) cell has weight 1)
 # array: axis 0 row, axis 1 column, numerification starts at 0
- 
 X = np.array([
     [1,1],
     [1,1],
     [1,1]
 ])
 
-u = np.array([TGTunder50, TGT51to80, TGTover80]) # row target
-v = np.array([TGTmale, TGTfemale]) # col target
+u = np.array([TGTunder50, TGT51to80, TGTover80]) # row target (age)
+v = np.array([TGTmale, TGTfemale]) # col target (gender)
 
-# row
-print(f'observed: {X.sum(axis=1)}')
-print(f'target: {u}')
 
-# columns
-print(f'observed: {X.sum(axis=0)}')
-print(f'target: {v}')
-
+# ipf algorithm
+# run by row first then column
 def ipf_update(M, u, v):
     r_sums = M.sum(axis=1)
     N = np.array([[M[r,c] * u[r] / r_sums[r] for c in range(M.shape[1])]
@@ -139,17 +130,18 @@ def ipf_update(M, u, v):
     O = np.array([[N[r, c] * v[c] / c_sums[c] for c in range(N.shape[1])]
                   for r in range(N.shape[0])])
 
-    d_u = np.linalg.norm(u - O.sum(axis=1), 2)
+    d_u = np.linalg.norm(u - O.sum(axis=1), 2)   # l2-norm as validation measure (vectorial difference between target and fitter values)
     d_v = np.linalg.norm(v - O.sum(axis=0), 2)
 
     return O, d_u, d_v
 
-M = X.copy()
+M = X.copy() # to set the dataset to run the IPF algorithm over
 
+# compute algorithm
 for _ in range(10):
     M, d_u, d_v = ipf_update(M, u, v)
     print(f'd_u = {d_u:.5f}, d_v = {d_v:.5f}')
-    if d_u <= 0.0001 and d_v <= 0.0001:
+    if d_u <= 0.0001 and d_v <= 0.0001:          # algorithm stops if the distance below threshold
         break
 
 M
@@ -166,10 +158,23 @@ def percsample(T):
                   for r in range(T.shape[0])])
     return  P
 
-N = percsample(M)
-N.sum(axis=0) # sum marginal columns
-N.sum(axis=1) # sum marginal rows
-N.sum()
+# percentage fitted
+Fp = percsample(M) 
+Fp.sum(axis=0) # sum marginal columns
+Fp.sum(axis=1) # sum marginal rows
+Fp.sum()
+
+# to add names row and columns
+Fpdf = pd.DataFrame(Fp, columns=['male','female'], index=['leq50','51to80','over80']) 
 
 
+# percentage target for comparison validation
+Tp = percsample(T) 
+Tp.sum(axis=0) # sum marginal columns
+Tp.sum(axis=1) # sum marginal rows
+Tp.sum()
 
+#  output csv (names columns and rows not appearing, to adjust format encoding in excel)
+
+os.chdir('C:/Users/rocpa/OneDrive/Desktop/ROME_CNR/WP5/WP5_FOSSR-main/WP5_FOSSR-main/')
+Fpdf.to_csv('percent_fitted.csv',sep = ",")
